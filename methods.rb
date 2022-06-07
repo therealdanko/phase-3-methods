b5a61cf5adb08c2340e8cde2e868602a60312edf
Danko Karleusa <dankomatichevich@gmail.com>
2022-06-07 10:05:48 -0700

    commit tree b1122dddee2022f7d5b48b062460cea962eb2622

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+ puts "Hello, programmer!"
+end
+
+def greet(name) 
+ puts "Hello, #{name}!" 
+end
+
+def greet_with_default(name="programmer")
+ puts "Hello, #{name}!"
+end
+
+def add(a, b)
+ a + b
+end
+
+def halve(a)
+ if a.class != Integer
+    nil
+ else 
+    a/2
+ end
+end
